58df63146f39343e4e2ba1f06d89555053e756c4
Resolute <resolute@maxocull.com>
2017-11-22 01:32:23 -0500

    Notifies via email, correctly watches and logs openings

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# purdue-slack-course-watcher
-Watches for class openings at Purdue and reports it to Slack.
+# Purdue Course Watcher
+
+Watches for class openings at Purdue and reports it to emails. This is
+especially useful when using SMS emails such as `2025550134@txt.att.net`, which
+will allow users to be notified of an opening immediately.
+
+## Setup
+
+This program requires `sendmail`
+
+```
+git clone https://github.com/Maxattax97/purdue-course-watcher
+npm install
+# add/edit watchlist with correct information
+node index.js
+```
+
+Simply tweak `watchlist` to add new classes to be watched. The format is
+simple:
+```
+# Comment
+
+<Code Spring/Summer/Fall, Year> <Course Registration Number> <Comma Separated List of Emails>
+```
+Examples are available in the default watchlist.
--- index.js
+++ index.js
@@ -1,16 +1,70 @@
-var SlackBot = require("slackbots");
+var fs = require("fs");
+var readline = require("readline");
 var Email = require("email").Email;
 var scrape = require("scrape");
 
-function main(args) {
-    if (args.length < 5) {
-        console.log("Usage: node index.js <term> <crn> <email>[,email,...]");
-        process.exit(0);
-    }
+var watchlist = [];
+var interval = 10;
 
-    getAvailability(args[2], args[3]).then(function(classObj) {
-        notifyEmail(args[4], classObj);
-    }).catch(console.error);
+function main(args) {
+    console.log("Reading watchlist ...");
+
+    var reader = readline.createInterface({
+        input: fs.createReadStream("watchlist")
+    });
+
+    var lineNum = 0;
+    reader.on("line", function(line) {
+        lineNum++;
+
+        if (line && line.charAt(0) != "#") {
+            var parts = line.split(" ");
+
+            var term = parts[0];
+            var crn = parts[1];
+            var emails = parts[2];
+
+            if (parts && crn && emails) {
+                watchlist.push({
+                    term: term,
+                    crn: crn,
+                    emails: emails,
+                    notified: false
+                });
+            } else {
+                console.log("Error on line " + lineNum + ": unexpected format (<term> <crn> <email list>)");
+            }
+        }
+    });
+
+    reader.on("close", function() {
+        console.log("Parsed " + watchlist.length + " courses into the watchlist"); //, watchlist);
+        console.log("Beginning watch ...");
+
+        checkOpenings();
+        setInterval(function() {
+            checkOpenings();
+        }, interval * 1000);
+    });
+}
+
+function checkOpenings() {
+    watchlist.forEach(function(item) {
+        getAvailability(item.term, item.crn).then(function(classObj) {
+            var timestamp =  "[" + (new Date().toISOString()) + "]";
+            if (classObj.remaining > 0) {
+                if (!item.notified) {
+                    console.log(timestamp + " " + classObj.shortName + " (" + classObj.longName + ") has " + classObj.remaining + " open seats. Notifying ...");
+                    notifyEmails(item.emails, classObj);
+                    item.notified = true;
+                } else {
+                    console.log(timestamp + " " + classObj.shortName + " (" + classObj.longName + ") has " + classObj.remaining + " open seats.");
+                }
+            } else {
+                item.notified = false;
+            }
+        }).catch(console.error);
+    });
 }
 
 function getAvailability(term, crn) {
@@ -20,7 +74,7 @@ function getAvailability(term, crn) {
                 if (err) {
                     reject(err);
                 } else {
-                    var className = $("table.datadisplaytable").find("th").first().text; //.innerText;
+                    var className = $("table.datadisplaytable").find("th").first().text;
                     var openSeats = parseInt($("td.dddefault")[3].children[0].data, 10);
                     if (!isNaN(openSeats)) {
                         resolve({longName: className.split(" - ")[0], shortName: className.split(" - ")[2], remaining: openSeats});
@@ -32,7 +86,7 @@ function getAvailability(term, crn) {
     });
 }
 
-function notifyEmail(recipient, classObj) {
+function notifyEmails(recipient, classObj) {
     var payload = new Email({
         from: "resolute@maxocull.com",
         to: recipient.split(","),
@@ -42,33 +96,10 @@ function notifyEmail(recipient, classObj) {
 
     // Sendmail must be available
     payload.send(function(err) {
-        console.error(err);
+        if (err) {
+            console.error(err);
+        }
     });
 }
 
-//var bot = new SlackBot({
-    //token: require("./token.json").value,
-    //name: "Course Watcher"
-//});
-
-//bot.on("start", function() {
-    //console.log("Course Watcher bot is running...");
-
-    //var params = {
-        //icon_emoji: ":mortar_board:"
-    //};
-
-    //bot.postMessageToUser("max", "Testing", params).fail(console.err);
-//});
-
-//bot.on("message", function(data) {
-    //if (data.type == "message") {
-        //var user = data.user;
-        //var text = data.text;
-        //console.log(data.user + ": " + data.text);
-    //} else {
-        //// console.log(data);
-    //}
-//});
-
-main(process.argv);
+main();
